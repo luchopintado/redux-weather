d379cd12b3e53aed2e3393b3f8d9a55d9ac2058d
luchopintado <luchopintado@gmail.com>
2017-08-29 11:48:50 -0500

    Redux-promise

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
 class SearchBar extends Component {
 
@@ -16,6 +19,10 @@ class SearchBar extends Component {
 
     onFormSubmit (evt) {
         evt.preventDefault();
+        console.log('onFormSubmit ...');
+
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
     }
 
     render() {
@@ -36,4 +43,8 @@ class SearchBar extends Component {
     }
 }
 
-export default SearchBar;
+function mapDispatchToProps (dispatch) {
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
