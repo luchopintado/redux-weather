2e22f21dd0bf70b347219be002ab59403b37545c
luchopintado <luchopintado@gmail.com>
2017-08-29 11:18:59 -0500

    Ajax request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
 // Section 5 - class 56
+import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = '3971a4d08052eb3580d0e0166d3a9982';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// q=Lima&
 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+export function fetchWeather (city) {
+    const url = `${ROOT_URL}&q=${city},pe`;
+    const request = axios.get(url);
 
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
